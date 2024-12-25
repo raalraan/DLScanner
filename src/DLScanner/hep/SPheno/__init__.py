8e2792aa4c49c8cc3ef4e38f200ef9146d08d9a6
Ahmed Hammad <68282212+AHamamd150@users.noreply.github.com>
2024-12-25 10:22:30 +0900

    Update __init__.py

--- src/DLScanner/hep/SPheno/__init__.py
+++ src/DLScanner/hep/SPheno/__init__.py
@@ -2,4 +2,4 @@ from SPheno import *
 from utils import *
 from vegas_S import *
 from SPheno_trial import ML_SL_trial
-from Spheno_trail_regressor import ML_regressor()
+from Spheno_trail_regressor import ML_regressor
