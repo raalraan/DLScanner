be969c7c1f9c06d5378e0dbbea9cb1cc676d2ba5
Ahmed Hammad <68282212+AHamamd150@users.noreply.github.com>
2024-12-25 10:34:17 +0900

    Update plot.py

--- src/DLScanner/utilities/plot.py
+++ src/DLScanner/utilities/plot.py
@@ -100,7 +100,7 @@ def plot_hist_all(file_:str,width= 15,hight=10, bins=100, Normalize=True, grid=T
 
 
 
-def plot_hist(file_:str,width= 15,hight=10, bins=100, Normalize=True, grid=True,col):
+def plot_hist(file_:str,width= 15,hight=10, bins=100, Normalize=True, grid=True,col:int):
   import numpy as np
   import matplotlib.pyplot as plt
   f= open(file_, 'r')
